6a3ddb09f015a9f63d3af0b2ef743b36776f7171
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-05-01 12:16:04 +0530

    Static rendering and serverside rendering

--- pages/[meetupid]/index.js
+++ pages/[meetupid]/index.js
@@ -1,14 +1,52 @@
 
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-const MeetupDetails = () =>{
+const MeetupDetails = () => {
     return <MeetupDetail
         image="https://cdn.pixabay.com/photo/2016/11/22/07/27/purdue-university-1848563_640.jpg"
         title="First Meetup"
-        address= "Some Street, new Dc"
+        address="Some Street, new Dc"
         description="First Meetup"
 
-     />
+    />
+}
+
+export async function getStaticPaths() {
+    return {
+        fallback: false,
+        paths: [{
+            params: {
+                meetupid: 'm1'
+            }
+        },
+        {
+            params: {
+                meetupid: 'm2'
+            }
+        },
+        {
+            params: {
+                meetupid: 'm3'
+            }
+        }]
+    }
+}
+
+export async function getStaticProps(context) {
+
+    const meetupId = context.params.meetupid;
+    console.log(meetupId);
+    return {
+        props: {
+            meetupdata: {
+                mage: "https://cdn.pixabay.com/photo/2016/11/22/07/27/purdue-university-1848563_640.jpg",
+                id: meetupId,
+                title: "First Meetup",
+                address: "Some Street, new Dc",
+                description: "First Meetup"
+            }
+        }
+    }
 }
 
 export default MeetupDetails;
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,55 @@
+
 import MeetupList from "../components/meetups/MeetupList";
 
 const Dummy_Array = [
     {
-        id:'m1',
-        title:'A first Mettup',
-        image:'https://cdn.pixabay.com/photo/2016/11/22/07/27/purdue-university-1848563_640.jpg',
+        id: 'm1',
+        title: 'A first Mettup',
+        image: 'https://cdn.pixabay.com/photo/2016/11/22/07/27/purdue-university-1848563_640.jpg',
         address: 'new Delhi ,2213432 ,new street',
         description: 'A First meet Here'
     },
     {
-        id:'m2',
-        title:'A second Mettup',
-        image:'https://cdn.pixabay.com/photo/2016/11/22/07/27/purdue-university-1848563_640.jpg',
+        id: 'm2',
+        title: 'A second Mettup',
+        image: 'https://cdn.pixabay.com/photo/2016/11/22/07/27/purdue-university-1848563_640.jpg',
         address: 'mumbai ,221312 ,new street',
         description: 'A second meet Here'
     },
     {
-        id:'m3',
-        title:'A Third Mettup',
-        image:'https://cdn.pixabay.com/photo/2016/11/22/07/27/purdue-university-1848563_640.jpg',
+        id: 'm3',
+        title: 'A Third Mettup',
+        image: 'https://cdn.pixabay.com/photo/2016/11/22/07/27/purdue-university-1848563_640.jpg',
         address: 'old Delhi ,2213431 ,oldstreet',
         description: 'A third meet Here'
     }
 ]
 
 
-const HomePage = ()=>{
-    return <MeetupList meetups={Dummy_Array}/>
-    
+const HomePage = (props) => {
+
+    return <MeetupList meetups={props.meetups} />
+
+}
+
+// export async function getServerSideProps(context) {
+//     const req = context.req;
+//     const res = context.res;
+
+//     return {
+//         props: {
+//             meetups: Dummy_Array
+//         }
+//     }
+// }
+
+export async function getStaticProps(){
+    return {
+        props: {
+            meetups: Dummy_Array
+        },
+        revalidate: 10
+    };
 }
 
 export default HomePage;
